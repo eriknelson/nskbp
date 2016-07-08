f75bfa771fe52138b5245908cc413ead14cfb45f
Erik Nelson <erik@nsk.io>
2016-07-08 19:32:16 -0400

    Move index.html to templates and load app

--- dev_srv.js
+++ dev_srv.js
@@ -14,7 +14,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'templates', 'index.html'));
 });
 
 app.listen(port, 'localhost', err => {
