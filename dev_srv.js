06e1200086e18016485c1e024e42f79e8f2aba5d
Erik Nelson <erik@nsk.io>
2016-07-08 19:07:31 -0400

    Initial dev_server

---
+++ dev_srv.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const config = require('./config/webpack.dev');
+
+const app = express();
+const compiler = webpack(config);
+const port = process.env.PORT || 3000;
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, 'localhost', err => {
+  if(err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(`Listening at http://localhost:${port}`);
+});
