8e3ce4b64dab4ba793af256bdcdaf7e9fe081737
Erik Nelson <erik@nsk.io>
2016-07-11 17:55:58 -0400

    Mock out some data

--- dev_srv.js
+++ dev_srv.js
@@ -7,14 +7,27 @@ const app = express();
 const compiler = webpack(config);
 const port = process.env.PORT || 3000;
 
+const dns = require('./mock_data/dns.json');
+const users = require('./mock_data/users.json');
+
 app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'templates', 'index.html'));
+});
+
+app.get('/api/v1/dns', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(dns));
+});
+
+app.get('/api/v1/users', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(users));
 });
 
 app.listen(port, 'localhost', err => {
