578a5a1c33c57539bde1dd37606217cf28cdb796
Erik Nelson <erik@nsk.io>
2016-07-12 00:17:55 -0400

    Initial commit of transition through app

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {
   Router, Route, IndexRoute, hashHistory
 } from 'react-router';
+import IntroPage from './auth/components/IntroPage';
 import Layout from './base/components/Layout';
 
 const Routes = () => {
   return (
     <Router history={hashHistory}>
-      <Route path="/" component={Layout} />
+      <Route path="/" component={IntroPage} />
+      <Route path="/home" component={Layout} />
     </Router>
   );
 };
