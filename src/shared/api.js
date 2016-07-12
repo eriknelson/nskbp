b619a66a6a997a952abc5b2fe4dfd9bacb28e932
Erik Nelson <erik@nsk.io>
2016-07-11 23:14:21 -0400

    Use correct api path

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,5 +1,5 @@
 const API_RESOURCE_BASE =
-  'http://localhost:3000/api/v1/'
+  'http://localhost:3000/api/v1'
 
 export function getResourceUrl(resource) {
   return `${API_RESOURCE_BASE}/${resource}`;
